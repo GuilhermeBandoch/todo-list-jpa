87d59191bb512eb75dee9be108690c1e828b9d5f
GUILHERME BANDOCH <68199@PROWAY.TREINA>
2020-11-22 18:01:03 -0300

    Model Teste

--- src/main/java/br/com/todolist/model/Teste.java
+++ src/main/java/br/com/todolist/model/Teste.java
@@ -3,38 +3,58 @@ package br.com.todolist.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Teste implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
-	private int cpf;
-	private Date dataNascimento;
 	
+	private String nome;
+	
+	private int cpf;
+	
+	private Date dataNascimento;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public int getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(int cpf) {
 		this.cpf = cpf;
 	}
+
 	public Date getDataNascimento() {
 		return dataNascimento;
 	}
+
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -42,6 +62,7 @@ public class Teste implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -58,6 +79,5 @@ public class Teste implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 }
